6c8be10a70646bd74f8db0dcf2adc8ccae957013
Param Singh <me@param.codes>
2023-03-26 14:30:10 +0530

    resize the images a bit

--- vault/assets/images/script.py
+++ vault/assets/images/script.py
@@ -1,7 +1,12 @@
 import os
+from PIL import Image
 
 for filename in os.listdir():
     if filename.endswith(".png"):
+        image = Image.open(filename)
+        image = image.resize((256, 256))
+        new_filename = f"{filename[:-4]}-resized.png"
+        image.save(new_filename)
         print(
-            f'<img src="/assets/images/{filename}" alt="{filename}" width="256px" height="256px">\n'
+            f'<a href="/assets/images/{filename}"><img src="/assets/images/{new_filename}" alt="{filename}" width="256px" height="256px" /></a>\n'
         )
